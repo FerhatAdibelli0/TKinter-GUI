2d6c4ce8de49293ad9429682b2bc6ca5dedffd73
Ferhat <ferhatadibelli9@gmail.com>
2024-08-30 01:04:06 +0300

    check diff

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ window = tkinter.Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
+#label
 
+my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
+my_label.pack()
 
 
 
